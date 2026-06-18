4c2b804ce7e36a6a684c56c3799a545db1a8bed4
cedri-i <1413085250@qq.com>
2026-06-18 16:07:06 +0800

    Rework notes navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,11 +32,87 @@ export default defineConfig({
             customCss: ['./src/styles/custom.css'],
             sidebar: [
                 {
-                    label: '计算机系统',
+                    label: 'My Notes',
                     items: [
                         {
-                            label: 'CMU: CSAPP',
-                            autogenerate: { directory: 'notes' },
+                            label: '导航',
+                            link: '/my-notes/',
+                        },
+                        {
+                            label: '计算机系统',
+                            collapsed: true,
+                            items: [
+                                {
+                                    label: 'Overview',
+                                    link: '/computer-systems/',
+                                },
+                                {
+                                    label: 'CMU: CSAPP',
+                                    collapsed: true,
+                                    items: [
+                                        {
+                                            label: 'Overview',
+                                            link: '/computer-systems/csapp/',
+                                        },
+                                        {
+                                            label: 'Lecture 01',
+                                            link: '/notes/lecture-01/',
+                                        },
+                                        {
+                                            label: 'Lecture 02',
+                                            link: '/notes/lecture-02/',
+                                        },
+                                        {
+                                            label: 'Lecture 03',
+                                            link: '/notes/lecture-03/',
+                                        },
+                                        {
+                                            label: 'Lecture 04',
+                                            link: '/notes/lecture-04/',
+                                        },
+                                        {
+                                            label: 'Lecture 05',
+                                            link: '/notes/lecture-05/',
+                                        },
+                                        {
+                                            label: 'Lecture 06',
+                                            link: '/notes/lecture-06/',
+                                        },
+                                        {
+                                            label: 'Lecture 07',
+                                            link: '/notes/lecture-07/',
+                                        },
+                                        {
+                                            label: 'Lecture 08',
+                                            link: '/notes/lecture-08/',
+                                        },
+                                        {
+                                            label: 'Lecture 09',
+                                            link: '/notes/lecture-09/',
+                                        },
+                                        {
+                                            label: 'Lecture 10',
+                                            link: '/notes/lecture-10/',
+                                        },
+                                        {
+                                            label: 'Lecture 11',
+                                            link: '/notes/lecture-11/',
+                                        },
+                                        {
+                                            label: 'Lecture 12',
+                                            link: '/notes/lecture-12/',
+                                        },
+                                        {
+                                            label: 'Lecture 13',
+                                            link: '/notes/lecture-13/',
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                        {
+                            label: '白鱼 n',
+                            link: '/baiyun/',
                         },
                     ],
                 },
